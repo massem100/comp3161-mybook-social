463399a27ac3f0b05c93a4a5763134329e03fa4c
Tonnika <44046572+Tonnika@users.noreply.github.com>
2020-04-27 04:25:42 -0500

    edits
    more edits on user id

--- dummy_data.py
+++ dummy_data.py
@@ -1,7 +1,7 @@
 from faker import Faker
 fake = Faker()
 
-fake.alphaNumeric()
+fake.userName()
 fake.first_name()
 fake.last_name()
 #fake.birthdate()
@@ -10,6 +10,6 @@ fake.password()
 fake.phoneNumberFormat()
 
 for _ in range(10):
-  print(fake.alphaNumeric(),fake.first_name(), fake.last_name(),fake.email(),
+  print(fake.userName(),fake.first_name(), fake.last_name(),fake.email(),
         fake.password(),fake.phoneNumberFormat())
 
