935aa39d66f423335a56adc4fcc582c273a5e1a0
Tonnika <44046572+Tonnika@users.noreply.github.com>
2020-04-27 02:59:24 -0500

    test
    inital testing of the dummy data file

---
+++ dummy_data.py
@@ -0,0 +1,12 @@
+from faker import Faker
+fake = Faker()
+
+fake.name()
+# 'Lucy Cechtelar'
+
+fake.address()
+# '426 Jordy Lodge
+#  Cartwrightshire, SC 88120-6700'
+
+for _ in range(10):
+  print(fake.name())
