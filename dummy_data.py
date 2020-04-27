8eb521650fe9939f4b5e5687f94391c5cda1489e
Tonnika <44046572+Tonnika@users.noreply.github.com>
2020-04-27 04:18:58 -0500

    edits to columns
    took out bithdate, needs more testing

--- dummy_data.py
+++ dummy_data.py
@@ -3,12 +3,12 @@ fake = Faker()
 
 fake.first_name()
 fake.last_name()
-fake.birthdate()
+#fake.birthdate()
 fake.email()
 fake.password()
 fake.phone_number()
 
 for _ in range(10):
-  print(fake.first_name(), fake.last_name(),fake.birthdate(),fake.email(),
+  print(fake.first_name(), fake.last_name(),fake.email(),
         fake.password(),fake.phone_number())
 
