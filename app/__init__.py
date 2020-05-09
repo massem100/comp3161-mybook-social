053e887e93ecc8a53bf42305db18e159d6fd5576
Orlando <orlando_aka_nick@hotmail.com>
2020-05-09 00:32:26 -0500

    group

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\x
 # password = 'pbkdf2:sha256:150000$uYDMjgNm$bd9a9c2ab56e93b8ed5ffe1d9a7e6cea2e849efc7ed7fb8f1f02c200360c71f8'
 # SQL DECLARE Statements, password should be hashed before passing it.
 # Statements below allow us to initicalize the MYSQL database
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'SQLpass'
 app.config['MYSQL_DB'] = 'mybook'
